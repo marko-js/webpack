ea0bbdeb22e6359fbaf2f440d68654241b16275c
Dylan Piercey <dpiercey@ebay.com>
2020-03-02 20:04:54 -0800

    fix: use cjs for Marko 4 and esm for 5 (#28)

--- src/__tests__/fixtures/basic-template-plugin/__snapshots__/browser--test_uYWJ.js
+++ src/__tests__/fixtures/basic-template-plugin/__snapshots__/browser--test_uYWJ.js
@@ -15,19 +15,15 @@
 /*!*************************************************************************!*\
   !*** ./src/__tests__/fixtures/basic-template-plugin/test.marko?hydrate ***!
   \*************************************************************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _test_marko_dependencies__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test.marko?dependencies */ "./src/__tests__/fixtures/basic-template-plugin/test.marko?dependencies");
-/* harmony import */ var _test_marko_dependencies__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_test_marko_dependencies__WEBPACK_IMPORTED_MODULE_0__);
 
       if (window.$mwp) {
         __webpack_require__.p = $mwp;
       }
 
-      
+      __webpack_require__(/*! ./test.marko?dependencies */ "./src/__tests__/fixtures/basic-template-plugin/test.marko?dependencies");
       window.$initComponents && window.$initComponents();
     
 
--- src/__tests__/fixtures/multiple-entries-plugin/__snapshots__/browser--foo_3XPO.js
+++ src/__tests__/fixtures/multiple-entries-plugin/__snapshots__/browser--foo_3XPO.js
@@ -4,14 +4,10 @@
 /*!*******************************************************************************!*\
   !*** ./src/__tests__/fixtures/multiple-entries-plugin/foo.marko?dependencies ***!
   \*******************************************************************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _components_shared_marko_dependencies__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/shared.marko?dependencies */ "./src/__tests__/fixtures/multiple-entries-plugin/components/shared.marko?dependencies");
-/* harmony import */ var _components_shared_marko_dependencies__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_shared_marko_dependencies__WEBPACK_IMPORTED_MODULE_0__);
-
+__webpack_require__(/*! ./components/shared.marko?dependencies */ "./src/__tests__/fixtures/multiple-entries-plugin/components/shared.marko?dependencies");
 
 /***/ }),
 
@@ -19,18 +15,15 @@ __webpack_require__.r(__webpack_exports__);
 /*!**************************************************************************!*\
   !*** ./src/__tests__/fixtures/multiple-entries-plugin/foo.marko?hydrate ***!
   \**************************************************************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _foo_marko_dependencies__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./foo.marko?dependencies */ "./src/__tests__/fixtures/multiple-entries-plugin/foo.marko?dependencies");
 
       if (window.$mwp) {
         __webpack_require__.p = $mwp;
       }
 
-      
+      __webpack_require__(/*! ./foo.marko?dependencies */ "./src/__tests__/fixtures/multiple-entries-plugin/foo.marko?dependencies");
       window.$initComponents && window.$initComponents();
     
 
