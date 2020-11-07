e69650ae34645f136d5ec2a93c7c98a278ff1f0b
Michael Rawlings <mirawlings@ebay.com>
2020-11-06 17:32:35 -0800

    feat: <webpack-assets> tag

---
+++ src/__tests__/fixtures/asset-tag/__snapshots__/browser--other.js
@@ -0,0 +1,14 @@
+/******/ ({
+
+/***/ "./src/__tests__/fixtures/asset-tag/other.js":
+/*!***************************************************!*\
+  !*** ./src/__tests__/fixtures/asset-tag/other.js ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+console.log("hello from other");
+
+/***/ })
+
+/******/ });
