5b0a78efc9b6e10a3a64b8bb4dd4c9d2afd489cb
Michael Rawlings <mirawlings@ebay.com>
2021-03-12 18:19:01 -0800

    chore: update webpack@5

--- src/__tests__/fixtures/asset-tag/__snapshots__/webpack5/server--main.js
+++ src/__tests__/fixtures/asset-tag/__snapshots__/webpack5/server--main.js
@@ -351,8 +351,9 @@ module.exports = require("marko/dist/runtime/html/helpers/attrs");;
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
